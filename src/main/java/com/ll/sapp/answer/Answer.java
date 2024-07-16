c75fd1cad93e327f07eb81ec0ef14d25ef6fb5cc
jeong-sys <20211939@edu.hanbat.ac.kr>
2024-07-16 14:03:02 +0900

    project

---
+++ src/main/java/com/ll/sapp/answer/Answer.java
@@ -0,0 +1,36 @@
+package com.ll.sapp.answer;
+
+import com.ll.sapp.question.Question;
+import com.ll.sapp.user.SiteUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+
+        @Column(columnDefinition = "TEXT")
+        private String content;
+
+        private LocalDateTime createDate;
+
+        @ManyToOne
+        private Question question;
+
+        @ManyToOne
+        private SiteUser author;
+
+        private LocalDateTime modifyDate;
+
+        @ManyToMany
+    Set<SiteUser> voter;
+}
